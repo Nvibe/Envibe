9a0e08c86f99d8c9452a3831840c69119660a52e
Joshua Zenn <wordman05@gmail.com>
2020-06-15 17:45:54 -0400

    Finished cache backbone
    - Created bean definitions for RedisTemplate<K,V>
    - Configured Spring to pull Redis connection string from master environment variable
    - Created wrapper Dao class to serve as basis for all cached objects

---
+++ src/main/java/com/envibe/envibe/config/RedisConfig.java
@@ -0,0 +1,43 @@
+package com.envibe.envibe.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisConnectionUtils;
+import org.springframework.data.redis.core.RedisTemplate;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() throws URISyntaxException {
+        URI redisURI = new URI(System.getenv("REDIS_URL"));
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisURI.getHost(), redisURI.getPort());
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(10);
+        poolConfig.setMaxIdle(5);
+        poolConfig.setMinIdle(1);
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setTestOnReturn(true);
+        poolConfig.setTestWhileIdle(true);
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(poolConfig);
+        jedisConnectionFactory.setHostName(redisURI.getHost());
+        jedisConnectionFactory.setPort(redisURI.getPort());
+        jedisConnectionFactory.setTimeout(Protocol.DEFAULT_TIMEOUT);
+        jedisConnectionFactory.setPassword(redisURI.getUserInfo().split(":", 2)[1]);
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() throws URISyntaxException {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}
---
+++ src/main/java/com/envibe/envibe/dao/CachedItemDao.java
@@ -0,0 +1,40 @@
+package com.envibe.envibe.dao;
+
+import com.envibe.envibe.model.CachedItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.validation.constraints.NotNull;
+
+@Repository
+public class CachedItemDao {
+    public static String PURPOSE_GENERAL_CACHE = "GENERAL";
+    public static String PURPOSE_NEWS_FEED_CACHE = "NEWS";
+
+    @Autowired
+    private RedisTemplate<String, CachedItem> redisTemplate;
+
+    public void create(@NotNull CachedItem cachedItem) {
+        redisTemplate.opsForValue().set(generateTag(cachedItem), cachedItem);
+    }
+    public CachedItem read(@NotNull String tag) {
+        return redisTemplate.opsForValue().get(tag);
+    }
+    public void update(@NotNull CachedItem cachedItem) {
+        delete(generateTag(cachedItem));
+        create(cachedItem);
+    }
+    public void delete(@NotNull CachedItem cachedItem) {
+        delete(generateTag(cachedItem));
+    }
+    public void delete(@NotNull String tag) {
+        redisTemplate.delete(tag);
+    }
+    private String generateTag(@NotNull String purpose, String user_tag) {
+        return purpose + "|" + user_tag;
+    }
+    private String generateTag(@NotNull CachedItem cachedItem) {
+        return generateTag(cachedItem.getPurpose(), cachedItem.getUserTag());
+    }
+}
