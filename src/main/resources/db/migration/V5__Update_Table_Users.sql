b44604980e1a2295a3f994b5f7bb687193358562
Joshua Zenn <wordman05@gmail.com>
2020-06-16 15:18:55 -0400

    Glued new frontend code to existing templates
    - Added Thymeleaf model injection support in new templates.
    - Moved /assets to static hosting folder.
    - Renamed templates to match Spring Security framework naming schema.
    - Created database migration for new fields.

---
+++ src/main/resources/db/migration/V5__Update_Table_Users.sql
@@ -0,0 +1,8 @@
+ALTER TABLE user_account ADD country varchar(100) NOT NULL;
+ALTER TABLE user_account ADD birthday DATE NOT NULL;
+ALTER TABLE user_account ADD last_name varchar(255) NOT NULL;
+ALTER TABLE user_account ADD first_name varchar(255) NOT NULL;
+
+UPDATE user_account SET country = 'United States', birthday = '2000-01-01', last_name = 'strator', first_name = 'admini' WHERE user_name = 'admin';
+UPDATE user_account SET country = 'United States', birthday = '2000-05-27', last_name = 'listnr', first_name = 'avid' WHERE user_name = 'listener';
+UPDATE user_account SET country = 'United States', birthday = '2005-11-05', last_name = 'band', first_name = 'bigname' WHERE user_name = 'artist';
