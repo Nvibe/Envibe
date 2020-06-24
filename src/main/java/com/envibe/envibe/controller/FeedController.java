7156a9b5236b8499b87f4f490f313e6ba88748b9
Joshua Zenn <wordman05@gmail.com>
2020-06-24 08:47:24 -0400

    Added newsfeed API endpoint

--- src/main/java/com/envibe/envibe/controller/FeedController.java
+++ src/main/java/com/envibe/envibe/controller/FeedController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Handles both browser requests and API calls related to user newsfeeds.
@@ -30,5 +31,24 @@ public class FeedController {
     @GetMapping("/feed")
     public String userFeed(Model model) {
         return "Feed";
+    }
+
+    /**
+     * Handles API requests for a user's cached newsfeed. URI follows the format /api/v1/feed?username=user1&count=10&after=4012.
+     * @param model Container we can use to inject data into the view.
+     * @param username Username to search for in cache tags.
+     * @param count Number of posts to return.
+     * @param after Last post_id received by the client.
+     * @return List of news items with specified parameters.
+     */
+    @GetMapping("/api/v1/feed")
+    public List<NewsItem> apiUserFeed(Model model, @RequestParam String username, @RequestParam(required = false) int count, @RequestParam(required = false) int after) {
+        if (count == 0) {
+            return newsFeedRetrievalService.getNewsFeed(username);
+        } else if (after == 0) {
+            return newsFeedRetrievalService.getNewsFeed(username, count);
+        } else {
+            return newsFeedRetrievalService.getNewsFeed(username, count, after);
+        }
     }
 }
