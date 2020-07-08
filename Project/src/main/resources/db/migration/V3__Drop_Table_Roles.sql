75eceb4a9744516dfd1a56ede668d9ae13e3f88f
Joshua Zenn <wordman05@gmail.com>
2020-07-08 09:33:50 -0400

    Moved all project files up
    - Moved all files in the Project directory up one level.
    - Renamed a migration file to match the naming convention used by the rest of the codebase.

--- Project/src/main/resources/db/migration/V3__Drop_Table_Roles.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE user_account ADD user_role varchar(15) NOT NULL; -- Required by Spring Security framework.
-DROP TABLE user_role;
--- Project/target/classes/db/migration/V1_Friend_Table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS user_relationship (
-	user_name VARCHAR(255) NOT NULL,
-	user_friend VARCHAR(255) NOT NULL,
-	FOREIGN KEY (user_name) REFERENCES user_account(user_name)
-	FOREIGN KEY (user_friend) REFERENCES user_account(user_name)
-);
