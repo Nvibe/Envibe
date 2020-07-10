6464c3bb88ea4f945c104f627b073d30a04667ee
edeputy <62220210+edeputy@users.noreply.github.com>
2020-07-10 14:41:54 -0400

    Add files via upload

--- src/main/java/com/envibe/envibe/dao/FriendDao.java
+++ src/main/java/com/envibe/envibe/dao/FriendDao.java
@@ -1,14 +1,20 @@
 package com.envibe.envibe.dao;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
+import javax.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import com.envibe.envibe.model.Relationship;
 import com.envibe.envibe.rowmapper.FriendRowMapper;
 
+@Repository
 public class FriendDao {
+	 @Autowired
+	
+	 /* injects JdbcTemplate into class*/
 	private JdbcTemplate jdbcTemplate;
 	
 	final String queryCreate = "INSERT INTO user_relationship (user_name, user_friend) VALUES (?, ?)";
@@ -19,29 +25,30 @@ public class FriendDao {
 	
 	final String queryDelete = "DELETE FROM user_relationship WHERE user_name = ?";
 	
+	/*General CRUD format, update not actually necessary but for "Future Features" and the possibility of needing it in case users update their userNames than its just there*/
+	
 	public void create(Relationship relation) {
         Objects.requireNonNull(relation, "Method argument relation cannot be null");
         jdbcTemplate.update(queryCreate, relation.getUserName(), relation.getUserFriend());
     }
 	
-	public List<String[]> read(String userName) {
+	/*Creates a list of objects of Relationship.*/
+	public List<Relationship> read(@NotNull String userName) {
         Objects.requireNonNull(userName, "Method argument userName cannot be null");
         try {
-            return jdbcTemplate.queryForList(queryRead, String[]{userName}, new FriendRowMapper());
+        	return jdbcTemplate.query(queryRead, new FriendRowMapper(), userName);
         } catch (EmptyResultDataAccessException e) {
             return null;
         }
     }
 	
+	/*Unused but could be eventually.*/
 	public void update(Relationship relation) {
         Objects.requireNonNull(relation, "Method argument user cannot be null");
-        try {
         jdbcTemplate.update(queryUpdate, relation.getUserName(), relation.getUserFriend());
-        } catch (Exception e) {
-        	return;
-        }
     }
 	
+	/*Used to delete record from database, call a relationship object and than delete it in query.*/
 	public void delete(Relationship relation) {
         Objects.requireNonNull(relation, "Method argument relation cannot be null");
         jdbcTemplate.update(queryDelete, relation.getUserName());
