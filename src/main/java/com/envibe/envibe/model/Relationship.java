3a28ba0efd4eb8102a86aadde3b681fd7322e4aa
edeputy <62220210+edeputy@users.noreply.github.com>
2020-07-10 14:42:18 -0400

    Add files via upload

--- src/main/java/com/envibe/envibe/model/Relationship.java
+++ src/main/java/com/envibe/envibe/model/Relationship.java
@@ -3,18 +3,24 @@ package com.envibe.envibe.model;
 
 
 public class Relationship {
-
+	
+	/*Creates parameters for internal use of users and friends*/
     private String userName;
     private String userFriend;
 
+    /*Empty initialization of constructor*/
 	public Relationship() {
 
     }
 	
+	/*Constructor for Relationship model*/
 	public Relationship(String userName, String userFriend) {
         this.userName = userName;
         this.userFriend = userFriend;
     }
+	
+	
+	/*Simple getters and setters for model*/
 	
 	public String getUserName()
     {
