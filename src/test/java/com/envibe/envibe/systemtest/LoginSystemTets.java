dbe975d83eb727649646c5ffe120c64d8ce373a3
Joshua Zenn <wordman05@gmail.com>
2020-07-15 17:04:47 -0400

    Updated run configuration to include new system tests

--- src/test/java/com/envibe/envibe/systemtest/LoginSystemTets.java
+++ src/test/java/com/envibe/envibe/systemtest/LoginSystemTets.java
@@ -4,9 +4,11 @@ import com.envibe.envibe.SystemTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class LoginSystemTets extends SystemTest {
 
     @Test
